9394bcbac10ba8fbab2eb0dcb56a12600e220977
Rupak samanta <rupaksamantainbox@gmail.com>
2024-03-24 12:18:16 +0530

    first commit

---
+++ models/ideas_model.js
@@ -0,0 +1,21 @@
+module.exports = {
+                1:{
+                   id : 1,
+                   idea_name : "Good Idea",
+                   author_name : "Rupak Samanta",
+                   idea_description : "My first Idea" 
+                },
+                2:{
+                    id : 2,
+                    idea_name : "Good Second Idea",
+                    author_name : "Puchu Samanta",
+                    idea_description : "My Second Idea" 
+                },
+                3:{
+                    id : 3,
+                    idea_name : "Bad Idea",
+                    author_name : "Rupak Samanta",
+                    idea_description : "My Bad Idea"
+                }
+        
+            }
